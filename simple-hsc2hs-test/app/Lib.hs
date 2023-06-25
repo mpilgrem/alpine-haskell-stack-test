01687086937ee40ed7b414bc766c28bea7486b0d
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2023-06-25 20:02:56 +0100

    Apply hsc2hs directly

--- simple-hsc2hs-test/app/Lib.hs
+++
@@ -1,9 +0,0 @@
-{-# LINE 1 "simple-hsc2hs-test\\app\\Lib.hsc" #-}
--- This *.hsc files does not use any of the features of hsc2hs.
-
-module Lib
-  ( magicInteger
-  ) where
-
-magicInteger :: Int
-magicInteger = 13
